bea2acbc50338a6609bb70d7322f696f08175605
john <johnchoi89@john-ubuntu-16.04>
2017-11-21 01:06:34 -0800

    added controller and index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'default/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root :to => 'index#index'
+#root :to => 'index#index'
 end
